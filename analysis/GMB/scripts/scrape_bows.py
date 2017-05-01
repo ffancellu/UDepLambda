52ddbe7046ca78e519f6b75da4583f49b7caad47
Federico Fancellu <ffancellu@cngl.ie>
2017-05-01 20:03:38 +0100

    scrape bows

--- analysis/GMB/scripts/scrape_bows.py
+++ analysis/GMB/scripts/scrape_bows.py
@@ -30,8 +30,11 @@ def analyze_page(par,doc,content):
     infos = []
     for row in bows:
         cols = row.find_all('td')
-        info = {'type_ann':cols[1].text,
-                'type_corr':cols[4].text,
+        info = {'time':cols[0].text,
+                'user':cols[2].text,
+                'type_ann':cols[1].text,
+                'type_corr1':cols[3].text,
+                'type_corr2':cols[4].text,
                 'text':cols[6].text}
         infos.append(info)
     _all = {'par':par,'doc':doc,'info':infos}
@@ -41,14 +44,17 @@ if __name__=="__main__":
     res_all = get_all_files(sys.argv[1],[])
     for filepath in res_all:
         par, doc = filepath.split('/')[-2:]
-            print "Processing %s :: %s " % (par,doc)
-            response = requests.get("http://gmb.let.rug.nl/explorer/explore.php?part=%s&doc_id=%s" % (par[1:],doc[1:])+\
-            "&type=bows&filter_part=&filter_status=A&filter_subcorpus=&filter_bows=&filter_warnings=")
-            entry = analyze_page(par,doc,response.content)
-            with codecs.open(sys.argv[2],'a','utf8') as out:
-                out.write('%%%s\t%s\n' % (entry['par'],entry['doc']))
-                for e in entry['info']:
-                    out.write('%s\t%s\t%s\n' % (e['type_ann'],
-                                            e['type_corr'],
-                                            e['text']))
-            time.sleep(1)
+        print "Processing %s :: %s " % (par,doc)
+        response = requests.get("http://gmb.let.rug.nl/explorer/explore.php?part=%s&doc_id=%s" % (par[1:],doc[1:])+\
+        "&type=bows&filter_part=&filter_status=A&filter_subcorpus=&filter_bows=&filter_warnings=")
+        entry = analyze_page(par,doc,response.content)
+        with codecs.open(sys.argv[2],'a','utf8') as out:
+            out.write('%%%s\t%s\n' % (entry['par'],entry['doc']))
+            for e in entry['info']:
+                out.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (e['time'],
+                                                        e['user'],
+                                                        e['type_ann'],
+                                                        e['type_corr1'],
+                                                        e['type_corr2'],
+                                                        e['text']))
+        time.sleep(1)
